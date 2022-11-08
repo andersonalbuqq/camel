566df82fc7945a76b1bc12eece58de06e8fe4af6
Elias Neto <eliasneto321@outlook.com>
2022-11-08 05:30:49 -0300

    Organizando a Arquitetura das Pastas e Arquivos

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,26 @@
 /* === HEADER OF PAGE ================================*/
+const drawer = document.querySelector("header .drawer")
+const overlay = document.querySelector(".overlay")
+const openMenuButton = document.querySelector(".openMenu")
+const closeMenuButton = document.querySelector(".closeMenu")
+const body = document.body
+let isOpen = false
+
 function openMenu() {
-  document.body.classList.add('menu-expanded')
-  document.getElementById("camada_bloq").style.display = "block"
+  drawer.classList.add("show")
+  overlay.classList.add("show")
+  closeMenuButton.style.display = "inherit"
+  openMenuButton.style.display = "none"
+  body.style.overflow = "hidden"
 }
 
 function closeMenu() {
-  document.body.classList.remove('menu-expanded')
-  document.getElementById("camada_bloq").style.display = "none"
+  drawer.classList.remove("show")
+  overlay.classList.remove("show")
+  openMenuButton.style.display = "inherit"
+  closeMenuButton.style.display = "none"
+  body.style.overflow = ""
 }
-
-let isOpen = false;
 
 function menu() {
   if (isOpen) {
@@ -20,8 +31,3 @@ function menu() {
     isOpen = true
   }
 }
-
-/* === PÁGINA HOME ================================*/
-$('.carousel').carousel({
-  interval: 5000
-})
