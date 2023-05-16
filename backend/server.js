e7b19d6094ffb61bd314e6aa38fa0238114d670c
Anderson <andersonmendes05@gmail.com>
2023-05-16 10:39:36 -0300

    commit inicial do backend

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+
+const webRouter = require("./routes/web");
+const public_path = "public";
+const views_path = "views";
+
+app.use(express.static(public_path));
+app.set("view engine", "ejs");
+app.set("views", [views_path]);
+app.use("/", webRouter);
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
