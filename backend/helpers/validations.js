2dd4db19c8a92885351e4e049cdb13805c09bf9a
Anderson <andersonmendes05@gmail.com>
2023-09-26 17:39:27 -0300

    inicio dos testes unitários da criação de produtos

---
+++ backend/helpers/validations.js
@@ -0,0 +1,124 @@
+const Subcategoria = require("../models/subcategoria");
+
+function validateName(name) {
+  if (!name) {
+    return {
+      status: 422,
+      message: "O nome é obrigatório.",
+      validationStatus: false,
+    };
+  }
+  return null;
+}
+
+function validatePrice(price) {
+  if (!price) {
+    return {
+      status: 422,
+      message: "O preço é obrigatório.",
+      validationStatus: false,
+    };
+  }
+
+  if (typeof(price) !== "number") {
+    return {
+      status: 422,
+      message: "Informe um valor numérico.",
+      validationStatus: false,
+    };
+  }
+
+  return null;
+}
+
+function validateDescription(description) {
+  if (!description) {
+    return {
+      status: 422,
+      message: "A descrição é obrigatória.",
+      validationStatus: false,
+    };
+  }
+
+  if (typeof(description) !== "string") {
+    return {
+      status: 422,
+      message: "Informe uma string",
+      validationStatus: false,
+    };
+  }
+
+  return null;
+}
+
+function validateDatasheet(datasheet) {
+  if (!datasheet) {
+    return {
+      status: 422,
+      message: "A ficha técnica é obrigatória.",
+      validationStatus: false,
+    };
+  }
+  return null;
+}
+
+function validateBrand(brand) {
+  if (!brand) {
+    return {
+      status: 422,
+      message: "A marca é obrigatória.",
+      validationStatus: false,
+    };
+  }
+  return null;
+}
+
+function validateAvailable(available) {
+  if (!available) {
+    return {
+      status: 422,
+      message: "A disponibilidade é obrigatória.",
+      validationStatus: false,
+    };
+  }
+
+  if (typeof available !== "boolean") {
+    return {
+      status: 422,
+      message: "É esperado um valor booleano.",
+      validationStatus: false,
+    };
+  }
+  return null;
+}
+
+async function validateSubcategory(subcategory) {
+  if (!subcategory) {
+    return {
+      status: 422,
+      message: "A subcategoria é obrigatória.",
+      validationStatus: false,
+    };
+  }
+
+  const hasSubcategory = await Subcategoria.findByPk(subcategory);
+  if (!hasSubcategory) {
+    return {
+      status: 404,
+      message: "Informe uma subcategoria válida",
+      validationStatus: false,
+    };
+  }
+
+  return null;
+}
+
+module.exports = {
+  validateName,
+  validatePrice,
+  validateDescription,
+  validateDatasheet,
+  validateBrand,
+  validateAvailable,
+  validateSubcategory,
+};
--- backend/tests/product.spec.js
+++ backend/tests/product.spec.js
@@ -1,19 +1,137 @@
-const ProductTestFunctions = require('./test-functions/product.js')
+const ProductTestFunctions = require("./test-functions/product.js");
 
 const formatMessageOutput = (func, parm, expectedStatus) => {
-  const { message, status } = func(parm, expectedStatus)
+  const result = func(parm);
 
-  return `${status ? '✅' : '❌'} ${message}`
-}
+  if (result === null) {
+    return `${expectedStatus === null ? "✅" : "❌"} Validation passed`;
+  }
+
+  const { validationStatus, message } = result;
+
+  return `${
+    validationStatus === expectedStatus ? "✅" : "❌"
+  } Erro: ${message}`;
+};
+
+const {
+  validateName,
+  validatePrice,
+  validateDescription,
+  validateDatasheet,
+  validateBrand,
+  validateAvailable,
+  validateSubcategory,
+} = require("../helpers/validations");
 
 module.exports = class ProductTests {
   static create() {
-    console.log('\nMódulo de Produtos - Criação')
-    console.log('----------------------------')
-    console.log('🚩 Validação de Nome')
-    console.log(`・ success with valid name: ${formatMessageOutput(ProductTestFunctions.create().validateNameExists, 'nome', true)}`)
-    console.log(`・ fail with empty string:  ${formatMessageOutput(ProductTestFunctions.create().validateNameExists, '', false)}`)
-    console.log(`・ fail with null value:  ${formatMessageOutput(ProductTestFunctions.create().validateNameExists, null, false)}`)
-    console.log(`・ fail with undefined value:  ${formatMessageOutput(ProductTestFunctions.create().validateNameExists, undefined, false)}`)
+    console.log("\nMódulo de Produtos - Criação");
+    console.log("----------------------------");
+    console.log("🚩 Validação de Nome");
+    console.log(
+      `・ success with valid name: ${formatMessageOutput(
+        validateName,
+        "Lustre",
+        null
+      )}`
+    );
+    console.log(
+      `・ fail with empty string: ${formatMessageOutput(
+        validateName,
+        "",
+        false
+      )}`
+    );
+    console.log(
+      `・ fail with null value: ${formatMessageOutput(
+        validateName,
+        null,
+        false
+      )}`
+    );
+    console.log(
+      `・ fail with undefined value: ${formatMessageOutput(
+        validateName,
+        undefined,
+        false
+      )}`
+    );
+
+    console.log("----------------------------");
+    console.log("🚩 Validação de Preço");
+    console.log(
+      `・ success with valid value: ${formatMessageOutput(
+        validatePrice,
+        156.8,
+        null
+      )}`
+    );
+    console.log(
+      `・ fail with unexpected value (string): ${formatMessageOutput(
+        validatePrice,
+        "cento e cinquenta",
+        false
+      )}`
+    );
+    console.log(
+      `・ fail with unexpected value (boolean): ${formatMessageOutput(
+        validatePrice,
+        true,
+        false
+      )}`
+    );
+    console.log(
+      `・ fail with null value: ${formatMessageOutput(
+        validatePrice,
+        null,
+        false
+      )}`
+    );
+    console.log(
+      `・ fail with undefined value: ${formatMessageOutput(
+        validatePrice,
+        undefined,
+        false
+      )}`
+    );
+
+    console.log("----------------------------");
+    console.log("🚩 Validação de Descrição");
+    console.log(
+      `・ success with valid value: ${formatMessageOutput(
+        validateDescription,
+        "Lustre ...",
+        null
+      )}`
+    );
+    console.log(
+      `・ fail with empty string: ${formatMessageOutput(
+        validateDescription,
+        "",
+        false
+      )}`
+    );
+    console.log(
+      `・ fail with unexpected value: ${formatMessageOutput(
+        validateDescription,
+        true,
+        false
+      )}`
+    );
+    console.log(
+      `・ fail with null value: ${formatMessageOutput(
+        validateDescription,
+        null,
+        false
+      )}`
+    );
+    console.log(
+      `・ fail with undefined value: ${formatMessageOutput(
+        validateDescription,
+        undefined,
+        false
+      )}`
+    );
   }
-}
+};
